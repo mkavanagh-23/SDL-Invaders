d46228fb715f3fcd2109a8fea791a1691097df7e
Matthew Kavanagh <matthewfkavanagh@icloud.com>
2024-11-17 16:30:42 -0500

    Add tilemap files

---
+++ test/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Background" tilewidth="32" tileheight="32" tilecount="54" columns="6">
+ <image source="tiles.bmp" trans="00ff00" width="192" height="288"/>
+</tileset>

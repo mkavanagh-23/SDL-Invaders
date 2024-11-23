8e9a6fd272b7bbb6db775bde834961c5fd1c03b7
Matthew Kavanagh <matthewfkavanagh@icloud.com>
2024-11-23 16:57:14 -0500

    Refactor objects to use global pointers

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
-#include <SDL2/SDL_keyboard.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_scancode.h>
-#include <SDL2/SDL_timer.h>
 #include <cstdlib>
 #include <ctime>
 #include <cassert>
 #include <iostream>
 #include <string>
+#include <SDL2/SDL.h>
 #include "engine.h"
 #include "types.h"
 #include "sprite.h"
@@ -36,9 +33,27 @@
 // Global Game Variables
 int playerScore = 0;
 int playerLives = 3;
+  bool playGame = false;
+
+// Declare Global game objects
+Background* background;
+Tilemap* tilemap;
+AnimatedSprite* logo;
+AnimatedSprite* player;
+AlienRow* topRow;
+AlienRow* upperRow;
+AlienRow* lowerRow;
+AlienRow* bottomRow;
 
 // Function Prototypes
+// Game state functions
 bool init();
+void end();
+void createObjects();
+void destroyObjects();
+// Gameplay functions
+void displayMenu();
+void startGame();
 
 /************************* MAIN GAME FUNCTION ***************************/
 int main() {
@@ -49,22 +64,7 @@ int main() {
     return 1;
   }
   
-  //Create game objects
-  Background background("graphics/bg.bmp");
-  Tilemap tilemap("graphics/tilemap.bmp");
-  AnimatedSprite logo("graphics/logo.bmp", 1, 0, "#000000");
-  logo.setLocation({ (settings::SCREEN_WIDTH - logo.getWidth()) / 2, -30 });
-  logo.update();
 
-  AnimatedSprite player("graphics/sprite.bmp", 16, 2, "#000000");
-  player.setSpeed(5);
-
-  AlienRow topRow(Rank::first);
-  AlienRow upperRow(Rank::second);
-  AlienRow lowerRow(Rank::third);
-  AlienRow bottomRow(Rank::fourth);
-
-  bool playGame = false;
   // BEGIN GAME LOOP
   while(SDL::ProgramIsRunning())
   {
@@ -77,57 +77,59 @@ int main() {
 
     // Check for left and right arrow keypresses
     if(keys[SDL_SCANCODE_LEFT]) {
-      player.setDirection(Direction::left);
-      player.move();
+      player->setDirection(Direction::left);
+      player->move();
     }
 
     if(keys[SDL_SCANCODE_RIGHT]) {
-      player.setDirection(Direction::right);
-      player.move();
+      player->setDirection(Direction::right);
+      player->move();
     }
 
+    // Display a menu until the player quits or selects 'START'
     if(!playGame) {
       if(keys[SDL_SCANCODE_SPACE]) {
         playGame = true;
-        background.scroll();
+        background->scroll();
         SDL_RenderClear(SDL::renderer);
-        background.draw();
-        logo.draw();
+        background->draw();
+        logo->draw();
         SDL_RenderPresent(SDL::renderer);
         SDL_Delay(1000);
         continue;
       }
-      player.nextFrame();
-      player.update();
-      background.scroll();
+      player->nextFrame();
+      player->update();
+      background->scroll();
       SDL_RenderClear(SDL::renderer);
-      background.draw();
-      logo.draw();
-      player.draw();
+      background->draw();
+      logo->draw();
+      player->draw();
       SDL_RenderPresent(SDL::renderer);
       SDL_Delay(20);
-    }
+    }   // END DISPLAY MENU
+   // Play the game 
     else {
-      player.nextFrame();
-      player.update();
-      topRow.update();
-      upperRow.update();
-      lowerRow.update();
-      bottomRow.update();
-      background.scroll();
+      player->nextFrame();
+      player->update();
+      topRow->update();
+      upperRow->update();
+      lowerRow->update();
+      bottomRow->update();
+      background->scroll();
       SDL_RenderClear(SDL::renderer);
-      background.draw();
-      tilemap.draw();
-      player.draw();
-      topRow.draw();
-      upperRow.draw();
-      lowerRow.draw();
-      bottomRow.draw();
+      background->draw();
+      tilemap->draw();
+      player->draw();
+      topRow->draw();
+      upperRow->draw();
+      lowerRow->draw();
+      bottomRow->draw();
       SDL_RenderPresent(SDL::renderer);
       SDL_Delay(20);
     }
   }
-  SDL::CloseShop();
+  end();
   return 0;
 }
 
@@ -140,5 +142,39 @@ bool init() {
   if(!Alien::init())
     return false;
 
+  //Create game objects
+  createObjects();
+
   return true;
+}
+
+void end() {
+  destroyObjects();
+  SDL::CloseShop();
+}
+
+void createObjects() {
+  background = new Background("graphics/bg.bmp");
+  tilemap = new Tilemap("graphics/tilemap.bmp");
+  logo = new AnimatedSprite("graphics/logo.bmp", 1, 0, "#000000");
+  player = new AnimatedSprite("graphics/sprite.bmp", 16, 2, "#000000");
+  topRow = new AlienRow(Rank::first);
+  upperRow = new AlienRow(Rank::second);
+  lowerRow = new AlienRow(Rank::third);
+  bottomRow = new AlienRow(Rank::fourth);
+  // Set object state
+  logo->setLocation({ (settings::SCREEN_WIDTH - logo->getWidth()) / 2, -30 });
+  logo->update();
+  player->setSpeed(5);
+}
+
+void destroyObjects() {
+  delete background;
+  delete tilemap;
+  delete logo;
+  delete player;
+  delete topRow;
+  delete upperRow;
+  delete lowerRow;
+  delete bottomRow; 
 }

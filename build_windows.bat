aaafea3603ee6cccb69ac54b409e71d5239632c8
mkavanagh-23 <154648803+mkavanagh-23@users.noreply.github.com>
2024-12-30 13:41:36 -0500

    Update build_windows.bat

--- build_windows.bat
+++ build_windows.bat
@@ -3,9 +3,10 @@
 echo Creating SDL-Invaders build files...
 cmake -B build
 
+echo
 echo Building SDL2 and SDL-Invaders...
 cmake --build build --config Release
 
 echo Build complete! Run SDL-Invaders.exe to play!
-echo Press any key to exit...
+echo
 pause
